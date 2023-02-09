a4f0c41dccf103f70ef32b5f08aaaedeacf5606a
Andrei Muntean <account@andreimuntean.dev>
2023-02-09 21:22:34 +0200

    Add dependecy to useEffect for handling edge cases

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,16 @@
-import '../styles/globals.css'
-import '../styles/burger.css'
-import '../styles/loading.css'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import { detectAdblock } from 'utils/AdBlockDetect'
+import "../styles/globals.css"
+import "../styles/burger.css"
+import "../styles/loading.css"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+import { detectAdblock } from "utils/AdBlockDetect"
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
 
   useEffect(() => {
-    detectAdblock((usingAdblock) => usingAdblock && router.push('/adblock-detected'))
-  }, [])
+    detectAdblock((usingAdblock) => usingAdblock && router.push("/adblock-detected"))
+  }, [router])
 
   return <Component {...pageProps} />
 }
--- pages/adblock-detected.jsx
+++ pages/adblock-detected.jsx
@@ -1,29 +1,36 @@
-import { useEffect } from 'react'
-import Head from 'next/head'
-import Button from 'components/Button'
-import Text from 'components/Text'
-import Wrapper from 'components/Wrapper'
-import { useRouter } from 'next/router'
-import { detectAdblock } from 'utils/AdBlockDetect'
+import { useEffect } from "react"
+import Head from "next/head"
+import Button from "components/Button"
+import Text from "components/Text"
+import Wrapper from "components/Wrapper"
+import { useRouter } from "next/router"
+import { detectAdblock } from "utils/AdBlockDetect"
 
 function AdblockDetected() {
   const router = useRouter()
 
   useEffect(() => {
-    detectAdblock((usingAdblock) => !usingAdblock && router.push('/'))
-  }, [])
-  
+    detectAdblock((usingAdblock) => !usingAdblock && router.push("/"))
+  }, [router])
+
   return (
     <>
       <Head>
-        <title>pearOS - Adblock detected</title> 
-      </Head> 
+        <title>pearOS - Adblock detected</title>
+      </Head>
 
       <Wrapper>
-        <div className='min-h-screen flex flex-col items-center justify-center'>
-          <Text type='title' styles='mb-4 text-center'>Oops, looks like you have Adblock on</Text>
-          <Text type='paragraph' styles='mb-8 sm:text-base max-w-lg text-center opacity-75'>This project is free to use, but servers, hosts, domains cost money, so please, help this project and disable your AdBlock or whitelist our website</Text>
-          <Button type='primary' onClick={() => router.reload(window.location.pathname)}>I disabled it</Button>
+        <div className="min-h-screen flex flex-col items-center justify-center">
+          <Text type="title" styles="mb-4 text-center">
+            Oops, looks like you have Adblock on
+          </Text>
+          <Text type="paragraph" styles="mb-8 sm:text-base max-w-lg text-center opacity-75">
+            This project is free to use, but servers, hosts, domains cost money, so please, help this project and
+            disable your AdBlock or whitelist our website
+          </Text>
+          <Button type="primary" onClick={() => router.reload(window.location.pathname)}>
+            I disabled it
+          </Button>
         </div>
       </Wrapper>
     </>
